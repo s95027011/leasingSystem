531c83a0722f4b27e41b2b329c47528d3d01ed36
MartinX1002 <top9451002@gmail.com>
2022-12-05 13:05:02 +0800

    add viewset

--- src/leasingSystem/leasing/views.py
+++ src/leasingSystem/leasing/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from leasing.models import Type, Product, Item, Transaction, Member, Cart, Order, Duerecord
+from leasing.serializers import leasingSerializer
 # Create your views here.
+
+
+class TypeViewSet(viewsets.ModelViewSet):
+    queryset = Type.objects.all()
+    serializer_class = leasingSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = leasingSerializer
+
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = leasingSerializer
+
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = leasingSerializer
+
+
+class MemberViewSet(viewsets.ModelViewSet):
+    queryset = Member.objects.all()
+    serializer_class = leasingSerializer
+
+
+class CartViewSet(viewsets.ModelViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = leasingSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = leasingSerializer
+
+
+class DuerecordViewSet(viewsets.ModelViewSet):
+    queryset = Duerecord.objects.all()
+    serializer_class = leasingSerializer
