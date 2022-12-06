39c1702864a2ddaf8d255720cadcf1ed737bb0f1
cico426426 <cico426426@gmail.com>
2022-12-06 09:20:19 +0800

    api 初步模板完成

--- src/leasingSystem/leasing/serializers.py
+++ src/leasingSystem/leasing/serializers.py
@@ -1,42 +1,42 @@
 from rest_framework import serializers
 from leasing.models import Type, Product, Item, Transaction, Member, Cart, Order, Duerecord
 
-class TypeSerializer(serializers.Serializer):
+class TypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Type
-        fields = '__all__'
+        fields = ('id', 'name')
 
-class ProductSerializer(serializers.Serializer):
+class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
 
-class ItemSerializer(serializers.Serializer):
+class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = '__all__'
 
-class TransactionSerializer(serializers.Serializer):
+class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
         fields = '__all__'
 
-class MemberSerializer(serializers.Serializer):
+class MemberSerializer(serializers.ModelSerializer):
     class Meta:
         model = Member
         fields = '__all__'
 
-class CartSerializer(serializers.Serializer):
+class CartSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cart
         fields = '__all__'
 
-class OrderSerializer(serializers.Serializer):
+class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = '__all__'
 
-class DuerecordSerializer(serializers.Serializer):
+class DuerecordSerializer(serializers.ModelSerializer):
     class Meta:
         model = Duerecord
         fields = '__all__'
