0878c8a945f35ca9461e68b32bd7824ddcd86bc9
cico426426 <cico426426@gmail.com>
2022-12-13 18:32:01 +0800

    add api

--- src/leasingSystem/leasing/views.py
+++ src/leasingSystem/leasing/views.py
@@ -27,9 +27,9 @@ class ItemViewSet(  mixins.CreateModelMixin,
         available = request.query_params.get('available', None)
         query = ''
         if available == 'True': # 上架
-            query = Item.objects.raw('SELECT * FROM leasing_item WHERE product_id = %s AND item_status = 0', [product_id])
+            query = Item.objects.all().filter(product_id__in=product_id).filter(item_status__in = '0')
         elif available == 'False': #以出租
-            query = Item.objects.raw('SELECT * FROM leasing_item WHERE product_id = %s AND item_status = 1', [product_id])
+            query = Item.objects.all().filter(product_id__in=product_id).filter(item_status__in = '1')
         serializer = ItemSerializer(query, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
@@ -37,7 +37,7 @@ class ItemViewSet(  mixins.CreateModelMixin,
     def list_item_by_product(self, request):
         product_id = request.query_params.get('product_id', None)
     
-        query = Item.objects.raw('SELECT * FROM leasing_item WHERE product_id = %s', [product_id])
+        query = Item.objects.all().filter(product_id__in=product_id)
         serializer = ItemSerializer(query, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
@@ -53,33 +53,66 @@ class ItemViewSet(  mixins.CreateModelMixin,
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class TransactionViewSet(   mixins.CreateModelMixin,
+                            mixins.UpdateModelMixin,
                             mixins.RetrieveModelMixin,
                             mixins.DestroyModelMixin,
                             viewsets.GenericViewSet):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
-    
-    @action(detail=True, methods="POST")
-    def change_transaction(self, request):
-        
-        pass
 
 class MemberViewSet(viewsets.ModelViewSet):
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
 
-class CartViewSet(viewsets.ModelViewSet):
+class CartViewSet(  mixins.CreateModelMixin,
+                    mixins.RetrieveModelMixin,
+                    mixins.DestroyModelMixin,
+                    viewsets.GenericViewSet):
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
+
+    def list(self, request):
+        member_id = request.query_params.get('member_id', None)
+        query = Cart.objects.all().filter(member_id__in = member_id)
+        serializer = CartSerializer(query, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    
+    def update(self, request, pk=None):
+        count = request.data['product_count']
+        query = ''
+        if count == 0:
+            query = self.destroy(self, request, pk=pk)
+        elif count > 0:
+            model = get_object_or_404(Member, pk=pk)
+            query = Member.objects.filter(pk=model.pk).update(request.data)
+        serializer = MemberSerializer(query)
+        if serializer.is_valid:
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        # return a meaningful error response
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, methods='post')
+    def clear_cart(self, request):
+        member_id = request.data['member_id']
+        query = Cart.objects.all().filter(member_id__in = member_id).delete()
+        serializer = CartSerializer(query)
+        if serializer.is_valid:
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        # return a meaningful error response
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
-    @action(detail=False, methods="POST")
+    @action(detail=False, methods="post")
     def list_order_by_member(self, request):
         member = request.data['member_id']
-        query = Item.objects.raw("SELECT * FROM leasing_order WHERE member_id = %s", [member])
+        query = Item.objects.all().filter(member_id__in = member)
         serializer = OrderSerializer(query, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
