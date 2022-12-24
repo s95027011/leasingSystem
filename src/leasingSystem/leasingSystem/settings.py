ab0d5c8b7bfe5328e0fa6faa33103ad602752b72
MartinX1002 <top9451002@gmail.com>
2022-12-24 15:54:57 +0800

    add setting

--- src/leasingSystem/leasingSystem/settings.py
+++ src/leasingSystem/leasingSystem/settings.py
@@ -61,6 +61,9 @@ ROOT_URLCONF = 'leasingSystem.urls'
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+
 )
 
 TEMPLATES = [
